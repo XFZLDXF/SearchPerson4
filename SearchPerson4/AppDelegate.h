5ce812bd568aacd5d2b387b5a9c1b53245121d7d
新风作浪 <duxinfeng99@gmail.com>
2013-05-04 21:29:47 +0800

    Initial Commit

---
+++ SearchPerson4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SearchPerson4
+//
+//  Created by DolBy on 13-5-4.
+//  Copyright (c) 2013年 Duxinfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
